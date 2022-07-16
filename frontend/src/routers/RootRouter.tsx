af6078dc34bedc6297cd80f2be12ab3621d512fb
염진호 <pwlsghq@naver.com>
2022-07-17 00:12:42 +0900

    feat : 메인페이지 루트 레이아웃 적용

--- frontend/src/routers/RootRouter.tsx
+++ frontend/src/routers/RootRouter.tsx
@@ -13,30 +13,33 @@ import Welcome from "@screens/Join/Welcome";
 import Login from "@screens/Login/Login";
 import ResetPw from "@screens/Login/ResetPw";
 import FindPw from "@screens/Login/FindPw";
+import "./RootLayout.scss";
 
 function RootRouter() {
   return (
     <>
       <MainNavBar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/tip" element={<TipPage />} />
-        <Route path="/feed" element={<FeedPage />} />
-        <Route path="/deal" element={<DealPage />} />
-        <Route path="/join/*">
-          <Route index element={<Join />} />
-          <Route path="chkEmail" element={<ChkEmail type="login" />} />
-          <Route path="detail" element={<JoinDetail />} />
-          <Route path="more" element={<JoinMore />} />
-          <Route path="welcome" element={<Welcome />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/find/pw">
-          <Route index element={<FindPw />} />
-          <Route path="chkEmail" element={<ChkEmail type="findPw" />} />
-        </Route>
-        <Route path="/reset/pw" element={<ResetPw />} />
-      </Routes>
+      <div id="root-layout">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/tip" element={<TipPage />} />
+          <Route path="/feed" element={<FeedPage />} />
+          <Route path="/deal" element={<DealPage />} />
+          <Route path="/join/*">
+            <Route index element={<Join />} />
+            <Route path="chkEmail" element={<ChkEmail type="login" />} />
+            <Route path="detail" element={<JoinDetail />} />
+            <Route path="more" element={<JoinMore />} />
+            <Route path="welcome" element={<Welcome />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/find/pw">
+            <Route index element={<FindPw />} />
+            <Route path="chkEmail" element={<ChkEmail type="findPw" />} />
+          </Route>
+          <Route path="/reset/pw" element={<ResetPw />} />
+        </Routes>
+      </div>
     </>
   );
 }
