7af92b5548541192b1e46bac8d877d14a49c4fb9
염진호 <pwlsghq@naver.com>
2022-07-17 12:44:09 +0900

    feat : 회원가입 페이지 루트 레이아웃 적용

--- frontend/src/screens/ChkEmail.tsx
+++ frontend/src/screens/ChkEmail.tsx
@@ -13,55 +13,59 @@ function ChkEmail({ type }: ChkEmailProps) {
     if (type === "findPw") navigate("/reset/pw");
   };
   return (
-    <div id="chk-email">
-      <header className="header">
-        <p className="header__title notoBold fs-24">
-          인증 이메일을 보냈습니다.
-        </p>
-        <p className="header__sub-title notoReg fs-16">
-          jinhoJJANG@gamile.com 에서 이메일을 확인 후 인증코드를 입력해주세요!
-        </p>
-      </header>
-      <main className="form">
-        <p className="form__title notoMid fs-16">인증코드</p>
-        <input
-          type="text"
-          className="form__input notoReg fs-15"
-          placeholder="인증번호를 입력해주세요"
-        />
-        <p className="form__msg notoMid fs-12">인증코드가 올바르지 않습니다.</p>
-        <button
-          className="form__btn notoMid fs-16 flex align-center justify-center"
-          type="button"
-          onClick={chkCode}
-        >
-          다음
-        </button>
-      </main>
-      <footer className="footer">
-        <div className="footer-top flex align-center justify-center">
-          <p className="footer-top__msg notoMid fs-12">
-            인증 이메일을 받지 못하셨나요?
+    <div className="wrapper">
+      <div id="chk-email">
+        <header className="header">
+          <p className="header__title notoBold fs-24">
+            인증 이메일을 보냈습니다.
           </p>
-          <button className="footer-top__btn notoBold fs-12" type="button">
-            이메일 재전송
+          <p className="header__sub-title notoReg fs-16">
+            jinhoJJANG@gamile.com 에서 이메일을 확인 후 인증코드를 입력해주세요!
+          </p>
+        </header>
+        <main className="form">
+          <p className="form__title notoMid fs-16">인증코드</p>
+          <input
+            type="text"
+            className="form__input notoReg fs-15"
+            placeholder="인증번호를 입력해주세요"
+          />
+          <p className="form__msg notoMid fs-12">
+            인증코드가 올바르지 않습니다.
+          </p>
+          <button
+            className="form__btn notoMid fs-16 flex align-center justify-center"
+            type="button"
+            onClick={chkCode}
+          >
+            다음
           </button>
-        </div>
-        <div className="footer-bottom">
-          <ul className="notice-ul notoMid fs-12">
-            <li className="notice-ul__li">
-              인증 이메일은 발송 시점으로 부터 3분간 유효합니다.
-            </li>
-            <li className="notice-ul__li">
-              인증 이메일 재발송 시 기존 인증코드는 무효처리되며, 새로 받은
-              인증코드로 인증해야 합니다.
-            </li>
-            <li className="notice-ul__li">
-              이메일이 도착하지 않았다면 스팸메일함을 확인해주세요.
-            </li>
-          </ul>
-        </div>
-      </footer>
+        </main>
+        <footer className="footer">
+          <div className="footer-top flex align-center justify-center">
+            <p className="footer-top__msg notoMid fs-12">
+              인증 이메일을 받지 못하셨나요?
+            </p>
+            <button className="footer-top__btn notoBold fs-12" type="button">
+              이메일 재전송
+            </button>
+          </div>
+          <div className="footer-bottom">
+            <ul className="notice-ul notoMid fs-12">
+              <li className="notice-ul__li">
+                인증 이메일은 발송 시점으로 부터 3분간 유효합니다.
+              </li>
+              <li className="notice-ul__li">
+                인증 이메일 재발송 시 기존 인증코드는 무효처리되며, 새로 받은
+                인증코드로 인증해야 합니다.
+              </li>
+              <li className="notice-ul__li">
+                이메일이 도착하지 않았다면 스팸메일함을 확인해주세요.
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
--- frontend/src/screens/Join/JoinMore.tsx
+++ frontend/src/screens/Join/JoinMore.tsx
@@ -45,51 +45,53 @@ function JoinMore() {
     open({ onComplete: handleComplete });
   };
   return (
-    <div id="join-more">
-      <header className="header">
-        <p className="header__title notoBold fs-24">추가 선택사항</p>
-        <p className="header__sub-title notoReg fs-16">
-          추가 정보를 입력하시면
-          <br />더 나은 서비스를 이용하실 수 있습니다.
-        </p>
-      </header>
-      <main className="form">
-        <p className="form__title notoBold fs-16">주소</p>
-        <input
-          type="text"
-          className="form__input fs-15 notoReg"
-          placeholder="주소를 검색해주세요"
-          readOnly
-          value={address}
-        />
-        <button
-          type="button"
-          className="form__btn fs-15 notoReg"
-          onClick={handleClick}
-        >
-          도로명 주소 검색
-        </button>
-        <p className="form__title notoBold fs-16">관심 카테고리</p>
-        <ul className="categorys-ul flex">
-          {categorys.map(category => (
-            <li
-              className="categorys-ul__li flex align-center justify-center notoReg fs-11"
-              key={v4()}
-            >
-              {category}
-            </li>
-          ))}
-        </ul>
-        <button
-          type="button"
-          className="form__btn--submit notoMid fs-16 flex align-center justify-center"
-          onClick={() => navigate("/join/welcome")}
-        >
-          다음
-        </button>
-      </main>
-      {/* {modalVisible && <AddressModal onClose={() => setModalVisible(false)} />}
-       */}
+    <div className="wrapper">
+      <div id="join-more">
+        <header className="header">
+          <p className="header__title notoBold fs-24">추가 선택사항</p>
+          <p className="header__sub-title notoReg fs-16">
+            추가 정보를 입력하시면
+            <br />더 나은 서비스를 이용하실 수 있습니다.
+          </p>
+        </header>
+        <main className="form">
+          <p className="form__title notoBold fs-16">주소</p>
+          <input
+            type="text"
+            className="form__input fs-15 notoReg"
+            placeholder="주소를 검색해주세요"
+            readOnly
+            value={address}
+          />
+          <button
+            type="button"
+            className="form__btn fs-15 notoReg"
+            onClick={handleClick}
+          >
+            도로명 주소 검색
+          </button>
+          <p className="form__title notoBold fs-16">관심 카테고리</p>
+          <ul className="categorys-ul flex">
+            {categorys.map(category => (
+              <li
+                className="categorys-ul__li flex align-center justify-center notoReg fs-11"
+                key={v4()}
+              >
+                {category}
+              </li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            className="form__btn--submit notoMid fs-16 flex align-center justify-center"
+            onClick={() => navigate("/join/welcome")}
+          >
+            다음
+          </button>
+        </main>
+        {/* {modalVisible && <AddressModal onClose={() => setModalVisible(false)} />}
+         */}
+      </div>
     </div>
   );
 }
