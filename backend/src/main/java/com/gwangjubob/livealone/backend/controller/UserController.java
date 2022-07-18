34ac7bbe1b0c58d384e93cd80d346916dd16abb8
lion1735 <lion1735@naver.com>
2022-07-18 14:01:37 +0900

    feat : 로그인 테스트 #15

--- backend/src/main/java/com/gwangjubob/livealone/backend/controller/UserController.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.gwangjubob.livealone.backend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class UserController {
+    private static final String okay = "SUCCESS";
+    @PostMapping("/user/login")
+    public ResponseEntity<?> refreshToken(@RequestParam("userId")String userId, HttpServletRequest request) throws Exception{
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status;
+        try {
+            resultMap.put("message",okay);
+            resultMap.put("userId",userId);
+            status = HttpStatus.ACCEPTED;
+        }catch (Exception e){
+            status = HttpStatus.UNAUTHORIZED;
+        }
+
+        return new ResponseEntity<>(resultMap, status);
+    }
 }
