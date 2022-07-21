9ff5e736ad37879356c64679a1d6da75a50180e5
hyejin-github <hyejineeeeee@gmail.com>
2022-07-21 11:07:38 +0900

    refactoring : 노티스 컨트롤러, 노티스 서비스 코드 리팩토링 #23

--- backend/src/main/java/com/gwangjubob/livealone/backend/service/DMService.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/service/DMService.java
@@ -3,7 +3,6 @@ package com.gwangjubob.livealone.backend.service;
 import com.gwangjubob.livealone.backend.dto.dm.DMSendDto;
 import com.gwangjubob.livealone.backend.dto.user.UserLoginDto;
 import com.gwangjubob.livealone.backend.dto.user.UserRegistDto;
-import com.gwangjubob.livealone.backend.dto.user.UserUpdateDto;
 
 public interface DMService {
     boolean sendDM(DMSendDto dmSendDto);
--- backend/src/main/java/com/gwangjubob/livealone/backend/service/NoticeService.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/service/NoticeService.java
@@ -1,19 +1,11 @@
 package com.gwangjubob.livealone.backend.service;
 
-import com.gwangjubob.livealone.backend.domain.entity.NoticeEntity;
-import com.gwangjubob.livealone.backend.domain.repository.NoticeRepository;
-import com.gwangjubob.livealone.backend.dto.notice.NoticeReadDto;
 import com.gwangjubob.livealone.backend.dto.notice.NoticeViewDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface NoticeService {
     List<NoticeViewDto> viewNotice(String id);
-
     void deleteNotice(String id, int idx);
-
-
     boolean readNotice(String decodeId, int idx);
 }
