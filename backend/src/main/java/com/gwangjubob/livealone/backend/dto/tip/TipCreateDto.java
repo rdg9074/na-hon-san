94ce0b368b3c7cd4eb0fb4553b11d1bdbf1c18f5
hyejin-github <hyejineeeeee@gmail.com>
2022-07-25 14:03:47 +0900

    feat : 꿀팁 관련 파일 생성 추가 #33

---
+++ backend/src/main/java/com/gwangjubob/livealone/backend/dto/tip/TipCreateDto.java
@@ -0,0 +1,30 @@
+package com.gwangjubob.livealone.backend.dto.tip;
+
+
+//// TipCreateDto - 게시글 작성 시 필요한 Entity
+//private User user; // 사용자 정보
+//private String category; // 해당 글 카테고리
+//private String title; // 글 제목
+//private String content; // 글 내용
+//private String bannerIme; // 배너 이미지 경로?
+//
+//@Builder
+//public Tip(User user, String category, String title, String content, String bannerImg){
+//        return Tip.builder()
+//        .User(user)
+//        .category(category)
+//        .title(title)
+//        .content(content)
+//        .build();
+//        }
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class TipCreateDto {
+
+}
