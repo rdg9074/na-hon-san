b9b12ffeaf3af0d6b772da5bf2685492b1d829b9
최강 <cg1735@naver.com>
2022-08-03 13:03:02 +0900

    [BE] 팔로우 꿀팁 게시글 조회, 기타 버그 수정

--- backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/UserFollowTipsRepository.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/UserFollowTipsRepository.java
@@ -3,6 +3,7 @@ package com.gwangjubob.livealone.backend.domain.repository;
 import com.gwangjubob.livealone.backend.domain.entity.TipEntity;
 import com.gwangjubob.livealone.backend.domain.entity.UserFollowEntity;
 import com.gwangjubob.livealone.backend.domain.entity.UserFollowTipsEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,8 +11,10 @@ import java.util.List;
 
 public interface UserFollowTipsRepository extends JpaRepository<UserFollowTipsEntity, String> {
 
-    @Query(value = "select * from tips t where user_id  In (SELECT follow_id  from user_follows where user_id = 'test')  ORDER  by t.time desc",nativeQuery = true)
-    List<UserFollowTipsEntity> findTips(String id);
+    @Query(value = "select * from tips t where user_id  In (SELECT follow_id  from user_follows where user_id =:#{#id} )  ORDER  by t.time desc",
+            countQuery = "SELECT COUNT(*) FROM tips",
+            nativeQuery = true)
+    List<UserFollowTipsEntity> findTips(String id, Pageable pageable);
 
 
 }
