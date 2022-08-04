3ba103dd8658dff657b11360cd1d9ed8b2de2449
정지원 <5653-stopone2639@users.noreply.lab.ssafy.com>
2022-08-04 11:43:33 +0900

    fix : 꿀딜 커서페이징 오류 수정   #34

--- backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/DealRepository.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/DealRepository.java
@@ -1,10 +1,7 @@
 package com.gwangjubob.livealone.backend.domain.repository;
 
 import com.gwangjubob.livealone.backend.domain.entity.DealEntity;
-import com.gwangjubob.livealone.backend.domain.entity.PopularDealEntity;
-import com.gwangjubob.livealone.backend.domain.entity.PopularFollowEntity;
 import com.gwangjubob.livealone.backend.domain.entity.UserEntity;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -26,27 +23,33 @@ public interface DealRepository extends JpaRepository<DealEntity, Integer> {
     Optional<DealEntity> findTop1ByOrderByViewDesc();
 
     List<DealEntity> findByCategory(String category);
-    Slice<DealEntity> findByStateAndIdxLessThanAndViewLessThanEqual(String state, Integer idx, Integer view, Pageable pageable);
+    @Query("select d from DealEntity d where d.state = :state and ((d.idx < :idx and d.view = :view  ) or d.view < :view)")
+    Slice<DealEntity> findView(String state, Integer idx, Integer view, Pageable pageable); //조회순
+    @Query("select d from DealEntity d where d.state = :state and ((d.idx < :idx and d.likes = :likes) or d.likes < :likes)")
+    Slice<DealEntity> findlikes(String state, Integer idx, Integer likes, Pageable pageable); //좋아요순
+    @Query("select d from DealEntity d where d.state = :state and d.idx < :idx")
+    Slice<DealEntity> findIdx(String state , Integer idx, Pageable pageable); //최신순
 
-    Slice<DealEntity> findByStateAndIdxLessThanAndLikesLessThanEqual(String state, Integer idx, Integer likes, Pageable pageable);
-    Slice<DealEntity> findByStateAndIdxLessThan(String state , Integer idx, Pageable pageable);
+    @Query("select d from DealEntity d where d.state = :state and d.title like %:keyword% and ((d.idx < :idx and d.view = :view) or d.view < :view)")
+    Slice<DealEntity> findTitleView(String state, String keyword, Integer idx, Integer view, Pageable pageable); //검색어, 조회순
+    @Query("select d from DealEntity d where d.state = :state and d.title like %:keyword% and ((d.idx < :idx and d.likes = :likes) or d.likes < :likes)")
+    Slice<DealEntity> findTitleLikes(String state, String keyword, Integer idx, Integer likes, Pageable pageable); //검색어 좋아요순
+    @Query("select d from DealEntity d where d.state = :state and d.title like %:keyword% and d.idx < :idx")
+    Slice<DealEntity> findTitleIdx(String state, String keyword, Integer idx, Pageable pageable); //검색어 최신순
 
-    Slice<DealEntity> findByStateAndTitleContainsAndIdxLessThanAndViewLessThanEqual(String state, String keyword, Integer idx, Integer view,Pageable pageable);
-    Slice<DealEntity> findByStateAndTitleContainsAndIdxLessThanAndLikesLessThanEqual(String state, String keyword, Integer idx, Integer likes,Pageable pageable);
-    Slice<DealEntity> findByStateAndTitleContainsAndIdxLessThan(String state, String keyword, Integer idx, Pageable pageable);
 
-
-
-    Slice<DealEntity> findByStateAndCategoryInAndIdxLessThanAndViewLessThanEqual(String state, List<String> categorys, Integer idx, Integer view, Pageable pageable);
-
-    Slice<DealEntity> findByStateAndCategoryInAndIdxLessThanAndLikesLessThanEqual(String state, List<String> categorys, Integer idx, Integer likes, Pageable pageable);
-    Slice<DealEntity> findByStateAndCategoryInAndIdxLessThan(String state, List<String> categorys, Integer idx, Pageable pageable);
-
-    Slice<DealEntity> findByStateAndCategoryInAndTitleContainsAndIdxLessThanAndViewLessThanEqual(String state, List<String> categorys, String keyword, Integer idx, Integer view, Pageable pageable);
-
-    Slice<DealEntity> findByStateAndCategoryInAndTitleContainsAndIdxLessThanAndLikesLessThanEqual(String state, List<String> categorys, String keyword, Integer idx, Integer likes, Pageable pageable);
-
-    Slice<DealEntity> findByStateAndCategoryInAndTitleContainsAndIdxLessThan(String state, List<String> categorys, String keyword, Integer idx, Pageable pageable);
+    @Query("select d from DealEntity d where d.state = :state and d.category in (:categorys) and ((d.idx < :idx and d.view = :view) or d.view < :view)")
+    Slice<DealEntity> findCategoryView(String state, List<String> categorys, Integer idx, Integer view, Pageable pageable); //카테고리 조회순
+    @Query("select d from DealEntity d where d.state = :state and d.category in (:categorys) and ((d.idx < :idx and d.likes = :likes) or d.likes < :likes)")
+    Slice<DealEntity> findCategoryLikes(String state, List<String> categorys, Integer idx, Integer likes, Pageable pageable); // 카테고리 좋아요순
+    @Query("select d from DealEntity d where d.state = :state and d.category in (:categorys) and d.idx < :idx")
+    Slice<DealEntity> findCategoryIdx(String state, List<String> categorys, Integer idx, Pageable pageable); //카테고리 최신순
+    @Query("select d from DealEntity d where d.state = :state and d.category in (:categorys) and d.title like %:keyword% and((d.idx < :idx and d.view = :view) or d.view < :view)")
+    Slice<DealEntity> findCategoryTitleView(String state, List<String> categorys, String keyword, Integer idx, Integer view, Pageable pageable); //카테고리 검색어 조회순
+    @Query("select d from DealEntity d where d.state = :state and d.category in (:categorys) and d.title like %:keyword% and((d.idx < :idx and d.likes = :likes) or d.likes < :likes)")
+    Slice<DealEntity> findCategoryTitleLikes(String state, List<String> categorys, String keyword, Integer idx, Integer likes, Pageable pageable);//카테고리 검색어 좋아요순
+    @Query("select d from DealEntity d where d.state = :state and d.category in (:categorys) and d.title like %:keyword% and d.idx < :idx")
+    Slice<DealEntity> findCategoryTitleIdx(String state, List<String> categorys, String keyword, Integer idx, Pageable pageable); //카테고리 검색어 최신순
 
     int countByUserId(String id);
 }
