b2d7823956e821e56212f0cc4dc032d4a0be62a2
Yeom-JinHo <pwlsghq@naver.com>
2022-08-10 08:54:45 +0900

    fix : BASE_URL

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "https://i7c208.p.ssafy.io:8083/api";
+// export const BASE_URL = "http://i7c208.p.ssafy.io:8083/api"; // 개발 주소
+export const BASE_URL = "/api";
 
 const API = axios.create({
   baseURL: BASE_URL,
