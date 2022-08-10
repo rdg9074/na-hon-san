0cb0f104269d96a3e0678c9e64301024124f334a
염진호 <pwlsghq@naver.com>
2022-08-10 15:38:53 +0900

    feat : 액세스토큰 재발급

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { config } from "process";
+import { refreshAccessToken } from "./auth";
 
 // export const BASE_URL = "http://i7c208.p.ssafy.io:8083/api"; // 개발 주소
 export const BASE_URL = "/api";
@@ -11,5 +13,21 @@ const API = axios.create({
     withCredentials: true
   }
 });
+
+API.interceptors.response.use(
+  response => {
+    return response;
+  },
+  err => {
+    console.log("intercept", err);
+    if (err.response.status === 401) {
+      refreshAccessToken();
+      const accessToken = sessionStorage.getItem("access-token") as string;
+      err.config.headers = { Authorization: `${accessToken}` };
+      return API(err.config);
+    }
+    return Promise.reject(err);
+  }
+);
 
 export default API;
