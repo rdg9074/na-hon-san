812b9e3b2d427e37c953e4678020630a39eacbfc
정지원 <5653-stopone2639@users.noreply.lab.ssafy.com>
2022-08-12 17:38:39 +0900

    [BE] bug : DM 리스트 조회 에러 수정 #90

--- backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/DMRepository.java
+++ backend/src/main/java/com/gwangjubob/livealone/backend/domain/repository/DMRepository.java
@@ -21,11 +21,11 @@ public interface DMRepository extends JpaRepository<DMEntity, String> {
     @Query(value = "select d from DMEntity d where (d.idx < :lastIdx and d.toUserId=:toId and d.fromUserId=:fromId)or(d.idx < :lastIdx and d.toUserId=:fromId and d.fromUserId=:toId) order by d.time desc")
     Slice<DMEntity> findByToUserIdAndFromUserId(UserEntity toId, UserEntity fromId, Integer lastIdx, Pageable pageable);
 
-    @Query(value = "select d from DMEntity d where d.fromUserId=:userId group by d.toUserId")
-    List<DMEntity> findByfromUserIdGrouptoUserId(UserEntity userId);
+    @Query(value = "select d.toUserId from DMEntity d where d.fromUserId=:userId group by d.toUserId")
+    List<UserEntity> findByfromUserIdGrouptoUserId(UserEntity userId);
 
-    @Query(value = "select d from DMEntity d where d.toUserId=:userId group by d.fromUserId")
-    List<DMEntity> findBytoUserIdGroupFromUserId(UserEntity userId);
+    @Query(value = "select d.toUserId from DMEntity d where d.toUserId=:userId group by d.fromUserId")
+    List<UserEntity> findBytoUserIdGroupFromUserId(UserEntity userId);
 
     @Query(value = "select d from DMEntity d where d.idx = (select max(d2.idx) from DMEntity d2 where (d2.fromUserId = :userId and d2.toUserId = :otherId) or (d2.fromUserId= :otherId and d2.toUserId = :userId))")
     Optional<DMEntity> findByUserIdAndOtherId(UserEntity userId, UserEntity otherId);
